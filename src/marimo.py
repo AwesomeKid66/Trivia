20d2c421bc4e71cf9cde8a1324a9077eef6d5dd8
Michael Okrzesik <mj3.14okrzesik@sbcglobal.net>
2025-08-13 16:47:16 -0500

    feat: updated to work with the sqlite database

--- src/marimo.py
+++ src/marimo.py
@@ -1,7 +1,7 @@
 import marimo
 
-__generated_with = '0.14.17'
-app = marimo.App(width='medium')
+__generated_with = "0.14.17"
+app = marimo.App(width="medium")
 
 
 @app.cell
@@ -16,27 +16,39 @@ def _():
 def _():
     import sqlalchemy
 
-    DATABASE_URL = 'sqlite:///data/database.db'
+    DATABASE_URL = "sqlite:///data/database.db"
     engine = sqlalchemy.create_engine(DATABASE_URL)
     return (engine,)
 
 
 @app.cell
-def _(sqf):
-    sqf.add_question()
-    return
-
-
-@app.cell
 def _(engine, mo):
     _df = mo.sql(
-        '''
+        """
         DELETE FROM questions where id=190
-        ''',
+        """,
         engine=engine
     )
     return
 
 
-if __name__ == '__main__':
+@app.cell
+def _(sqf):
+    questions = sqf.load_topic("Stranger Things")
+    return (questions,)
+
+
+@app.cell
+def _(questions):
+    for item in questions:
+        print(item)
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
     app.run()
